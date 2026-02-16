ee2209163e5b256e1ce7954dae0289cb35c37ef2
Dr. Yad Tahir <yad@ieee.org>
2026-02-16 09:27:30 +0300

    Java recap - Part 2

--- java-recap/ITE421.java
+++ java-recap/ITE421.java
@@ -1,6 +1,6 @@
 public class ITE421 {
 	void main() {
-		int x = 11;
+		// int x = 11;
 		// System.out.println("Hello world " + x);
 
 		// x = 011; // octa number
@@ -17,59 +17,79 @@ public class ITE421 {
 		// String name = "Ali";
 		// var name = "Ali";
 
-		int x = 20;
-		if (x > 0) {
-			// true
-			System.out.println(true);
-		} else {
-			// false
-			System.out.println(false);
-			System.out.println(false);
-			System.out.println(false);
-			System.out.println(false);
+		// int x = 20;
+		// if (x > 0) {
+		// // true
+		// System.out.println(true);
+		// } else {
+		// // false
+		// System.out.println(false);
+		// System.out.println(false);
+		// System.out.println(false);
+		// System.out.println(false);
+		// }
+
+		// int day = 1;
+		// int result = 0;
+
+		// int k = 20;
+		// if (k % 2 != 0) {
+		// // throw an error
+		// return;
+		// }
+		// // send email message
+		// // send email message
+		// // send email message
+		// // send email message
+		// // send email message
+		// // send email message
+		// // insert students data
+		// // insert students data
+		// // insert students data
+		// // insert students data
+		// // insert students data
+		// // insert students data
+
+		// switch (day) {
+		// case 0:
+		// result = 0;
+		// break;
+		// case 1:
+		// result = 10;
+		// break;
+		// case 2:
+		// result = 20;
+		// break;
+		// default:
+		// result = -1;
+		// }
+
+		// result = switch (day) {
+		// case 0 -> 0;
+		// case 1 -> 10;
+		// case 2 -> 20;
+		// default -> -1;
+		// };
+
+		// do while
+		long i = 1;
+		for (int j = 0; j < 20000; j++) {
+			for (; true;) {
+				if (i > 10_000) {
+					// continue;
+					// break;
+					return;
+				}
+
+				System.out.println("Hello AUIS " + i);
+				i += 1_000;
+			}
+			System.out.println(i);
 		}
 
-		int day = 1;
-		int result = 0;
-
-		int k = 20;
-		if (k % 2 != 0) {
-			// throw an error
-			return;
-		}
-		// send email message
-		// send email message
-		// send email message
-		// send email message
-		// send email message
-		// send email message
-		// insert students data
-		// insert students data
-		// insert students data
-		// insert students data
-		// insert students data
-		// insert students data
-
-		switch (day) {
-			case 0:
-				result = 0;
-				break;
-			case 1:
-				result = 10;
-				break;
-			case 2:
-				result = 20;
-				break;
-			default:
-				result = -1;
-		}
-
-		result = switch (day) {
-			case 0 -> 0;
-			case 1 -> 10;
-			case 2 -> 20;
-			default -> -1;
-		};
+		// H.W.
+		// for (String name : names) {
+		// }
 
 	}
 }
